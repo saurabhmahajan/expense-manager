899b4989e1a615874d9eaf6763659428ed542457
Saurabh Mahajan <saurabh7820@gmail.com>
2017-10-13 21:10:14 +0530

    code cleanup

--- ClientApp/src/categories/list.js
+++ ClientApp/src/categories/list.js
@@ -4,7 +4,6 @@ import CategoryService from '../services/category-service';
 @inject(CategoryService)
 export class List{
     constructor(categoryService){
-        this.category;
         this.categoryService = categoryService;
         this.moduleName = "Categories";
     }
@@ -16,8 +15,6 @@ export class List{
     }
 
     addCategory(){
-        console.log('Add Category');
-        console.log(this.category);
         return this.categoryService.add(this.category)
         .then(category => {
             this.categories.push(category);
