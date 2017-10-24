b7d293bec61bcb5e2bb88426c04adf1e551d0314
Saurabh Mahajan <saurabh7820@gmail.com>
2017-10-24 13:28:07 +0530

    font-awesome added

--- ClientApp/webpack.config.js
+++ ClientApp/webpack.config.js
@@ -28,6 +28,17 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test:/.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',
+                        publicPath: 'dist'
+                    }
+                }]
             }
         ]
     },
